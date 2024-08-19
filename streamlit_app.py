8b5ebaec1f2e98a6a0575fdeed15769ec1476f28
Karamjit Singh Bedi <74979722+why-kj-why@users.noreply.github.com>
2024-08-19 14:54:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -121,14 +121,13 @@ def store_ops_app():
     st.logo(image=image_data)
 
     # Claire Purple top bar on Top.
-    st.markdown(f"""
-    <div style="position: fixed; top: 0; left: 0; width: 100%; height: 100px; background-color: {CLAIRE_DEEP_PURPLE}; z-index: 1000;">
-        <p> Store Ops App </p>
-    </div>
-    """, unsafe_allow_html=True)
+    # st.markdown(f"""
+    # <div style="position: fixed; top: 0; left: 0; width: 100%; height: 100px; background-color: {CLAIRE_DEEP_PURPLE}; z-index: 1000;">
+    # </div>
+    # """, unsafe_allow_html=True)
 
     st.markdown(f"""
-    <h4 style="background-color: {CLAIRE_DEEP_PURPLE}; color: white; padding: 10px;">
+    <h4 style="position: fixed; top: 0; left: 0; width: 100%; height: 100px; background-color: {CLAIRE_DEEP_PURPLE}; color: white; padding: 10px;">
         Store Ops App
     </h4>
     """, unsafe_allow_html=True)
